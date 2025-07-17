3a6e3a912af5c3ce0f9138a2be683ecb1e14a662
piotr <piotrzych98@gmail.com>
2025-07-17 18:55:37 +0200

    update files

--- tests/test_ing_cookies.py
+++ tests/test_ing_cookies.py
@@ -1,53 +1,44 @@
 import pytest
 import os
-import asyncio
 from assertpy import assert_that
-from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
 from playwright_stealth import Stealth
 from utils.analytics_cookies import ANALYTICS_COOKIES
 
 
-@pytest.mark.asyncio
-async def test_ing_cookies():
-    async with Stealth().use_async(async_playwright()) as p:
+def test_ing_cookies():
+    with Stealth().use_sync(sync_playwright()) as p:
         for browser_type in [p.chromium, p.firefox, p.webkit]:
-            browser = await browser_type.launch(headless=True)
-            context = await browser.new_context()
-            page = await context.new_page()
+            browser = browser_type.launch(headless=True)
+            context = browser.new_context()
+            page = context.new_page()
 
-            await page.goto("https://www.ing.pl")
+            page.goto("https://www.ing.pl")
 
             if not os.path.exists("artifacts"):
                 os.makedirs("artifacts")
 
-            await page.screenshot(path="artifacts/screen_no_1.png")
-            # Dynamiczny wait, który pozwala, aby wszystko poprawnie wczytało się na stronie
-            await page.wait_for_load_state("load", timeout=60000)
+            page.screenshot(path="artifacts/screen_no_1.png")
+            page.wait_for_load_state("load", timeout=60000)
 
-            await page.screenshot(path="artifacts/screen_no_2.png")
-            # Wybranie odpowiednich cookies'ów
-            await page.get_by_role("button", name="Dostosuj").wait_for(state="visible", timeout=60000)
-            await page.get_by_role("button", name="Dostosuj").click()
-            await page.get_by_label("analityczne").check()
-            await page.get_by_role("button", name="Zaakceptuj zaznaczone").click()
+            page.screenshot(path="artifacts/screen_no_2.png")
 
-            # Wykonanie paru akcji na stronie, aby odpowiednie ciasteczka się wygenerowały
-            await page.get_by_role("button", name="Zaloguj").click()
-            await page.click('button[aria-label="Zamknij wyskakujące okno"]')
-            await page.click('a.small_teaser_tiles__anchor >> text="Pożyczka gotówkowa"')
+            page.get_by_role("button", name="Dostosuj").wait_for(state="visible", timeout=60000)
+            page.get_by_role("button", name="Dostosuj").click()
+            page.get_by_label("analityczne").check()
+            page.get_by_role("button", name="Zaakceptuj zaznaczone").click()
 
-            # Pobranie ciasteczek po wykonanych czynnościach
-            cookies = await context.cookies()
+            page.get_by_role("button", name="Zaloguj").click()
+            page.click('button[aria-label="Zamknij wyskakujące okno"]')
+            page.click('a.small_teaser_tiles__anchor >> text="Pożyczka gotówkowa"')
 
-            # Filtruj ciasteczka analityczne wg nazw
+            cookies = context.cookies()
+
             analytics_cookies = [c for c in cookies if c['name'] in ANALYTICS_COOKIES]
 
             assert_that(analytics_cookies).is_not_empty()
             for cookie in analytics_cookies:
-                
                 cookie_name = cookie['name']
                 assert_that(any(cookie_name.startswith(name) for name in ANALYTICS_COOKIES)).is_true()
 
-            await browser.close()
-
-asyncio.run(test_ing_cookies())
+            browser.close()
