094402f1c908b6f74d5baa6b548a7c9b5deffd08
piotr <piotrzych98@gmail.com>
2025-07-17 19:53:11 +0200

    update files

--- tests/test_ing_cookies.py
+++ tests/test_ing_cookies.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-import asyncio
 from assertpy import assert_that
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
@@ -10,34 +9,50 @@ from utils.analytics_cookies import ANALYTICS_COOKIES
 @pytest.mark.asyncio
 async def test_ing_cookies():
     errors = []
+
+    selected_browser = os.getenv("BROWSER")  # np. "chromium", "firefox", "webkit"
+
     async with Stealth().use_async(async_playwright()) as p:
-        for browser_type in [p.chromium, p.firefox, p.webkit]:
-            browser_name = browser_type.name
+        browser_map = {
+            "chromium": p.chromium,
+            "firefox": p.firefox,
+            "webkit": p.webkit,
+        }
+
+        # Jeśli nie podano konkretnej przeglądarki – testuj wszystkie
+        browsers_to_test = (
+            [selected_browser] if selected_browser in browser_map else browser_map.keys()
+        )
+
+        for browser_name in browsers_to_test:
+            browser_type = browser_map[browser_name]
             browser = None
             try:
                 browser = await browser_type.launch(headless=True)
                 context = await browser.new_context()
                 page = await context.new_page()
-                
+
                 await Stealth().apply_stealth_async(page)
 
-                await page.goto("https://www.ing.pl", timeout=60000)
+                await page.goto("https://www.ing.pl")
+                await page.wait_for_load_state("load", timeout=60000)
 
                 os.makedirs("artifacts", exist_ok=True)
                 await page.screenshot(path=f"artifacts/screen_no_1_{browser_name}.png")
-                await page.wait_for_load_state("load", timeout=60000)
                 await page.screenshot(path=f"artifacts/screen_no_2_{browser_name}.png")
 
-                # Cookie dialog
+                # Akceptacja cookies
                 await page.get_by_role("button", name="Dostosuj").wait_for(state="visible", timeout=60000)
                 await page.get_by_role("button", name="Dostosuj").click()
                 await page.get_by_label("analityczne").check()
                 await page.get_by_role("button", name="Zaakceptuj zaznaczone").click()
 
+                # Akcje na stronie
                 await page.get_by_role("button", name="Zaloguj").click()
                 await page.click('button[aria-label="Zamknij wyskakujące okno"]')
                 await page.click('a.small_teaser_tiles__anchor >> text="Pożyczka gotówkowa"')
 
+                # Sprawdzenie cookies
                 cookies = await context.cookies()
                 analytics_cookies = [c for c in cookies if c['name'] in ANALYTICS_COOKIES]
 
