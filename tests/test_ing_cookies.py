bc4ec1aa4ed1f8809296995cbed596a35708dd1f
piotr <piotrzych98@gmail.com>
2025-07-17 09:57:50 +0200

    update files

--- tests/test_ing_cookies.py
+++ tests/test_ing_cookies.py
@@ -1,48 +1,51 @@
 import pytest
 import os
+import asyncio
 from assertpy import assert_that
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
 from utils.analytics_cookies import ANALYTICS_COOKIES
 
 
-def test_ing_cookies():
-    with sync_playwright() as p:
+@pytest.mark.asyncio
+async def test_ing_cookies():
+    async with async_playwright() as p:
         # 
         for browser_type in [p.chromium, p.firefox, p.webkit]:
-            browser = browser_type.launch(headless=True)
-            context = browser.new_context()
-            page = context.new_page()
+            browser = await browser_type.launch(headless=False, slow_mo=100)
+            context = await browser.new_context()
+            page = await context.new_page()
 
-            Stealth().apply_stealth_sync(page)
+            await Stealth().apply_stealth_async(page)
 
-            page.goto("https://www.ing.pl")
+            await page.goto("https://www.ing.pl")
 
-            if not os.path.exists("artifacts"):
-                os.makedirs("artifacts")
+            # Dynamiczny wait, który pozwala, aby wszystko poprawnie wczytało się na stronie
+            await page.wait_for_load_state("networkidle")
 
             # Wybranie odpowiednich cookies'ów
-            page.screenshot(path="artifacts/01_main_page.png")
-            page.get_by_role("button", name="Dostosuj").wait_for(state="visible", timeout=60000)
-            page.screenshot(path="artifacts/02_after_trying_to_click_dostosuj.png")
-            page.get_by_role("button", name="Dostosuj").click()
-            page.get_by_label("analityczne").check()
-            page.get_by_role("button", name="Zaakceptuj zaznaczone").click()
+            await page.get_by_role("button", name="Dostosuj").wait_for(state="visible", timeout=60000)
+            await page.get_by_role("button", name="Dostosuj").click()
+            await page.get_by_label("analityczne").check()
+            await page.get_by_role("button", name="Zaakceptuj zaznaczone").click()
 
             # Wykonanie paru akcji na stronie, aby odpowiednie ciasteczka się wygenerowały
-            page.get_by_role("button", name="Zaloguj").click()
-            page.click('button[aria-label="Zamknij wyskakujące okno"]')
-            page.click('a.small_teaser_tiles__anchor >> text="Pożyczka gotówkowa"')
+            await page.get_by_role("button", name="Zaloguj").click()
+            await page.click('button[aria-label="Zamknij wyskakujące okno"]')
+            await page.click('a.small_teaser_tiles__anchor >> text="Pożyczka gotówkowa"')
 
-            cookies = context.cookies()
+            # Pobranie ciasteczek po wykonanych czynnościach
+            cookies = await context.cookies()
 
-            # 9. Filtruj ciasteczka analityczne wg nazw
+            # Filtruj ciasteczka analityczne wg nazw
             analytics_cookies = [c for c in cookies if c['name'] in ANALYTICS_COOKIES]
 
-            assert_that(analytics_cookies).is_not_empty().described_as("Brak ciasteczek analitycznych po akceptacji zgody.")
+            assert_that(analytics_cookies).is_not_empty()
             for cookie in analytics_cookies:
                 
                 cookie_name = cookie['name']
                 assert_that(any(cookie_name.startswith(name) for name in ANALYTICS_COOKIES)).is_true()
 
-            browser.close()
+            await browser.close()
+
+asyncio.run(test_ing_cookies())
